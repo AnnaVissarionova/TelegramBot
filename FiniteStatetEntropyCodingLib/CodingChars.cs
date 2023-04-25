c42e58e30a8eb115911ae2b580370b0bab3a5168
Виссарионова Анна <anna.ogneva.455@mail.ru>
2023-04-25 21:27:10 +0300

    -

--- FiniteStatetEntropyCodingLib/CodingChars.cs
+++ FiniteStatetEntropyCodingLib/CodingChars.cs
@@ -288,6 +288,7 @@ namespace FiniteStatetEntropyCodingLib
                         {
                             res[j] += last / (count - i);
                         }
+                        break;
                     }
                     i--;
                 }
